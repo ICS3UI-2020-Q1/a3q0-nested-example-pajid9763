23787f5dbafa964a0a7c0aa66fc4382b84a0f9a5
pajid9763 <pajid9763@wrdsb.ca>
2020-10-01 14:32:32 +0000

    finished more selection program

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+  Scanner input = new Scanner(System.in);
+
+  System.out.println("Does your animal have feathers?");
+  String feathers = input.nextLine();
+  if ( feathers.equals("yes") ) {
+    // animal has feathers
+    // ask the colour 
+    System.out.println("Are the feathers red or blue?");
+    String featherColour = input.nextLine();
+
+    // what is the colour
+    if( featherColour.equals("blue") ){
+      System.out.println("You have a blue jay");
+    }else if( featherColour.equals("red")){
+      System.out.println("You have a cardinal");
+    }
+  } else {
+    // animal doesn't have feathers 
     
   }
+}
 }
